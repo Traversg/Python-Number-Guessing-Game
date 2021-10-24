d8cf1cf372974684f316aff26d4b829751e1871d
Traversg <57022409+Traversg@users.noreply.github.com>
2021-10-23 21:22:29 -0500

    Add files via upload

--- guessing_game.py
+++ guessing_game.py
@@ -32,44 +32,49 @@ When the program starts, we want to:
 """
     # write your code inside this function.
 def start_game():    
-    
+
     scores = [10]
     
     while True:
         
+    
         print("*** Welcome to the Number Guessing Game!!! ***")
         high_score = min(scores)
         print(f"CURRENT HIGH SCORE: {high_score}")
         RAND_NUMBER = random.randint(1, 10)
         attempt_count = 1
+        guessed_number = 0
         
-        try:
-            guessed_number = input("Guess a number between 1 and 10: ")
-            guessed_number = int(guessed_number)
+        while guessed_number != RAND_NUMBER:    
+            try:
+                guessed_number = input("Guess a number between 1 and 10: ")
+                guessed_number = int(guessed_number)
             
-            if guessed_number > 10 or guessed_number < 1:
-                guessed_number = int(input("Sorry that number is out of range. Guess a number between 1 and 10: "))
-            
-            while guessed_number != RAND_NUMBER:
-                if guessed_number > RAND_NUMBER:
-                   guessed_number = int(input(f"Please guess again, number is lower than {guessed_number}: "))
-                   attempt_count += 1 
-                elif guessed_number < RAND_NUMBER:
-                   guessed_number = int(input(f"Please guess again, number is higher than {guessed_number}: "))
-                   attempt_count += 1
-        except ValueError:
-            print("Please enter a valid number")
-        
+                while guessed_number != RAND_NUMBER:
+                    try:
+                        if guessed_number > 10 or guessed_number < 1:
+                            guessed_number = int(input("Sorry that number is out of range. Guess a number between 1 and 10: "))
+                        elif guessed_number > RAND_NUMBER:
+                            guessed_number = int(input(f"Please guess again, number is lower than {guessed_number}: "))
+                            attempt_count += 1
+                        elif guessed_number < RAND_NUMBER:
+                            guessed_number = int(input(f"Please guess again, number is higher than {guessed_number}: "))
+                            attempt_count += 1
+                    except ValueError:
+                        print("Please enter a valid number.")
+            except ValueError:
+                print("Please enter a valid number.")
         
         if guessed_number == RAND_NUMBER:
                 print(f"Got it!! It took you {attempt_count} tries.\nThank you for playing!! ")
-                play_again = input("Would you like to play again? (Yes or No?) ")
+                play_again = input("Would you like to play again? (Yes or No) ")
                 if play_again.lower() == 'yes':
                     scores.append(attempt_count)
+                    
                 elif play_again.lower() == 'no':
                     print("Thank you for playing. Goodbye!")
                     break
-        
+    
 
         
         
